243f52262eb07f92b58e86a61430a5103fff8f55
Mathias Buus <mathiasbuus@gmail.com>
2017-07-31 14:49:58 -0700

    add cli

---
+++ cli/bin.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+var os = require('os')
+var path = require('path')
+var mkdirp = require('mkdirp')
+var proc = require('child_process')
+var minimist = require('minimist')
+
+var argv = minimist(process.argv.slice(2), {
+  alias: {
+    b: 'build-tools',
+    o: 'out'
+  },
+  default: {
+    o: 'app.apk'
+  }
+})
+
+if (!argv.b) {
+  console.error('--build-tools=[path/to/android/build/tools] is required')
+  process.exit(1)
+}
+
+var buildTools = argv.b
+var base = path.join(__dirname, 'base.apk')
+var tmp = path.join(os.tmpdir(), 'node-on-android-' + Date.now())
+var node = path.join(tmp, 'base', 'assets', 'node')
+var app = argv.o
+var keystore = path.join(__dirname, 'whatever.keystore')
+var cwd = process.cwd()
+
+mkdirp.sync(tmp)
+
+run('rm', ['-f', app])
+run('apktool', ['d', base])
+run('rm', ['-rf', node])
+run('cp', ['-rf', cwd, node])
+run('apktool', ['b', 'base', '-o', app])
+run(path.join(buildTools, 'zipalign'), ['-v', '-p', '4', app, app + '.aligned'])
+run('mv', [app + '.aligned', app])
+run(path.join(buildTools, 'apksigner'), ['sign', '--ks-pass', 'pass:whatever', '--ks', keystore, '--out', app, app])
+run('rm', ['-rf', tmp])
+
+console.log('Done! apk file is stored in:')
+console.log(path.resolve(cwd, app))
+
+function run (cmd, args) {
+  proc.spawnSync(cmd, args, {
+    cwd: tmp,
+    stdio: 'inherit'
+  })
+}
